44a92ae914b9894195cd38fac4e35f48b4e85b7e
Thiago099 <tecnologiasthiago@gmail.com>
2022-03-01 21:23:57 -0300

    [feature] base

---
+++ data.sql
@@ -0,0 +1,31 @@
+INSERT INTO `action` (`id`, `name`) VALUES
+	(1, 'hold'),
+	(2, 'consume'),
+	(3, 'pour'),
+	(4, 'drag');
+
+INSERT INTO `action_entities` (`action`, `entity`) VALUES
+	(2, 7),
+	(1, 8),
+	(3, 9),
+	(4, 8);
+
+INSERT INTO `action_parameters` (`action`, `entity`, `name`) VALUES
+	(3, 6, 'origin'),
+	(3, 6, 'recipient');
+
+INSERT INTO `entity` (`id`, `name`) VALUES
+	(1, 'glass'),
+	(2, 'milk'),
+	(6, 'container'),
+	(7, 'food'),
+	(8, 'solid'),
+	(9, 'fluid'),
+	(10, 'milk jug');
+    
+INSERT INTO `generalisation` (`parent`, `child`) VALUES
+	(6, 1),
+	(9, 2),
+	(8, 6),
+	(7, 2),
+	(6, 10);
---
+++ structure.sql
@@ -0,0 +1,38 @@
+CREATE TABLE IF NOT EXISTS `action` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `action_entities` (
+  `action` int(11) DEFAULT NULL,
+  `entity` int(11) DEFAULT NULL,
+  KEY `FK_power_entity` (`entity`),
+  KEY `FK__verb` (`action`) USING BTREE,
+  CONSTRAINT `FK__verb` FOREIGN KEY (`action`) REFERENCES `action` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_power_entity` FOREIGN KEY (`entity`) REFERENCES `entity` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `action_parameters` (
+  `action` int(11) DEFAULT NULL,
+  `entity` int(11) DEFAULT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  KEY `FK_action_entity_action` (`action`),
+  KEY `FK_action_entity_entity` (`entity`),
+  CONSTRAINT `FK_action_entity_action` FOREIGN KEY (`action`) REFERENCES `action` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_action_entity_entity` FOREIGN KEY (`entity`) REFERENCES `entity` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `entity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `generalisation` (
+  `parent` int(11) DEFAULT NULL,
+  `child` int(11) DEFAULT NULL,
+  KEY `FK__entity` (`parent`),
+  KEY `FK__entity_2` (`child`),
+  CONSTRAINT `FK__entity` FOREIGN KEY (`parent`) REFERENCES `entity` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK__entity_2` FOREIGN KEY (`child`) REFERENCES `entity` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
